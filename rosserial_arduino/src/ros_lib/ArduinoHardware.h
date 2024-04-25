2567d53b08a0861a7cf165a4bfbc7650e70ef6ed
racarla96 <racarla96@gmail.com>
2024-04-25 13:37:50 +0200

    Patch for support Arduino Due Programming Port as Default Option

--- rosserial_arduino/src/ros_lib/ArduinoHardware.h
+++ rosserial_arduino/src/ros_lib/ArduinoHardware.h
@@ -71,8 +71,12 @@ class ArduinoHardware {
     ArduinoHardware()
     {
 #if defined(USBCON) and !(defined(USE_USBCON))
-      /* Leonardo support */
-      iostream = &Serial1;
+      #if defined(_SAM3XA_)
+        iostream = &Serial;
+      #else
+        /* Leonardo support */
+        iostream = &Serial1;
+      #endif
 #elif defined(USE_TEENSY_HW_SERIAL) or defined(USE_STM32_HW_SERIAL)
       iostream = &Serial1;
 #else
